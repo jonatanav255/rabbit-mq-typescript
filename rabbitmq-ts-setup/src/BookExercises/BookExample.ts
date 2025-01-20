05aaead1487f7cdc63d0c7b437bc1ca414f8136d
Jonatan <jonatanav255@hotmail.com>
2025-01-19 19:30:49 -0600

    creating channel, and exchange

--- rabbitmq-ts-setup/src/BookExercises/BookExample.ts
+++ rabbitmq-ts-setup/src/BookExercises/BookExample.ts
@@ -3,7 +3,15 @@ import amqp from 'amqplib' // Import the RabbitMQ client library
 const connectToRabbitMQ = async () => {
   try {
     const url = 'amqp://guest:guest@localhost:5672/'
-    const connection  = await amqp.connect(url)
-    
+    const connection = await amqp.connect(url)
+    console.log('Connected to RabbitMQ')
+
+    const channel = await connection.createChannel()
+
+    const exchangeName = 'chapter2-example'
+
+    await channel.assertExchange(exchangeName, 'direct', { durable: true })
+    console.log(`Exchange '${exchangeName}' declared`);
   } catch {}
 }
+connectToRabbitMQ()
