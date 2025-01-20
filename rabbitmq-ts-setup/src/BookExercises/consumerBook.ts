1cc3fbba221a6b99e8bf9ac4a30fefa7f3ad201c
Jonatan <jonatanav255@hotmail.com>
2025-01-20 10:16:17 -0600

    testing hte message

--- rabbitmq-ts-setup/src/BookExercises/consumerBook.ts
+++ rabbitmq-ts-setup/src/BookExercises/consumerBook.ts
@@ -1,35 +1,37 @@
-import amqp from 'amqplib'; // Import the RabbitMQ client library
+import amqp from 'amqplib' // Import the RabbitMQ client library
 
 const receiveMessages = async () => {
   try {
     // RabbitMQ connection URL
-    const url = 'amqp://guest:guest@localhost:5672/';
+    const url = 'amqp://guest:guest@localhost:5672/'
 
     // Establish connection to RabbitMQ
-    const connection = await amqp.connect(url);
-    const channel = await connection.createChannel();
+    const connection = await amqp.connect(url)
+    const channel = await connection.createChannel()
 
-    const queueName = 'example';
+    const queueName = 'example'
 
     // Consume messages from the queue
-    channel.consume(queueName, (msg) => {
+    channel.consume(queueName, msg => {
       if (msg) {
-        // console.log(msg)
+        console.log('--------------')
+        console.log(msg)
+        console.log('--------------')
         console.log('Message received:');
         console.log(`ID: ${msg.properties.messageId}`);
         console.log(`Timestamp: ${new Date(msg.properties.timestamp).toISOString()}`);
         console.log(`Body: ${msg.content.toString()}`);
 
         // Acknowledge the message
-        channel.ack(msg);
+        channel.ack(msg)
       }
-    });
+    })
 
-    console.log(`Waiting for messages in queue: '${queueName}'`);
+    console.log(`Waiting for messages in queue: '${queueName}'`)
   } catch (error) {
-    console.error('Error receiving messages:', error);
+    console.error('Error receiving messages:', error)
   }
-};
+}
 
 // Execute the consumer function
-receiveMessages();
+receiveMessages()
