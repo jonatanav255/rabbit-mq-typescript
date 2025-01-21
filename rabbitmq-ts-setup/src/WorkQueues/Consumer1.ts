3acef9a155247953ea2e5152f68bd5da66553f4a
Jonatan <jonatanav255@hotmail.com>
2025-01-21 12:43:52 -0600

    creating consumer 1 file and connection

---
+++ rabbitmq-ts-setup/src/WorkQueues/Consumer1.ts
@@ -0,0 +1,11 @@
+import amqp from 'amqplib'
+
+const consumeTask = async (consumerName: string) => {
+  try {
+    const url = 'amqp://guest:guest@localhost:5672/'
+    const connection = await amqp.connect(url)
+
+    console.log(`[${consumerName}] Connected to Rabbit MQ`)
+    
+  } catch (error) {}
+}
